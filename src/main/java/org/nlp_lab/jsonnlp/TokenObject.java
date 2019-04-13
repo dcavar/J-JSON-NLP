6f9f2d854489c132991319c1f932f8f60b3c7481
Damir Cavar <dcavar@me.com>
2019-04-13 13:36:42 -0400

    Adding documentation to the Java code.

--- src/main/java/org/nlp_lab/jsonnlp/TokenObject.java
+++ src/main/java/org/nlp_lab/jsonnlp/TokenObject.java
@@ -50,134 +50,275 @@ public class TokenObject {
 		return id;
 	}
 
+	/**
+	 * This method sets the token ID.
+	 *
+	 * @param id This is the ID of the token.
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+	/**
+	 * This method returns the string representation of the token as found in the input text.
+	 *
+	 * @return string This is the string representation of the token as found in input text.
+	 */
 	public String getWord() {
 		return word;
 	}
 
+	/**
+	 * This method sets the string representation for the token as found in the input text.
+	 *
+	 * @param word This is the string representation of the token.
+	 */
 	public void setWord(String word) {
 		this.word = word;
 	}
 
+	/**
+	 * This method returns the lemma for the token. The lemma is the lexical base-form of the token.
+	 * This is usually the infinitival form of the verb or the nominative singular form of nouns.
+	 *
+	 * @return string This is the lemma of the token.
+	 */
 	public String getLemma() {
 		return lemma;
 	}
 
+	/**
+	 * This method sets the lemma for the token.
+	 *
+	 * @see org.nlp_lab.jsonnlp.TokenObject#getLemma()
+	 * @param lemma The lemma for the token.
+	 */
 	public void setLemma(String lemma) {
 		this.lemma = lemma;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public Integer getCharacterOffsetBegin() {
 		return characterOffsetBegin;
 	}
 
+	/**
+	 *
+	 * @param characterOffsetBegin
+	 */
 	public void setCharacterOffsetBegin(Integer characterOffsetBegin) {
 		this.characterOffsetBegin = characterOffsetBegin;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public Integer getCharacterOffsetEnd() {
 		return characterOffsetEnd;
 	}
 
+	/**
+	 *
+	 * @param characterOffsetEnd
+	 */
 	public void setCharacterOffsetEnd(Integer characterOffsetEnd) {
 		this.characterOffsetEnd = characterOffsetEnd;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public String getPOS() {
 		return POS;
 	}
 
+	/**
+	 *
+	 * @param POS
+	 */
 	public void setPOS(String POS) {
 		this.POS = POS;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public String getNER() {
 		return NER;
 	}
 
+	/**
+	 *
+	 * @param NER
+	 */
 	public void setNER(String NER) {
 		this.NER = NER;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public String getSpeaker() {
 		return speaker;
 	}
 
+	/**
+	 *
+	 * @param speaker
+	 */
 	public void setSpeaker(String speaker) {
 		this.speaker = speaker;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public Boolean getForeign() {
 		return foreign;
 	}
 
+	/**
+	 *
+	 * @param foreign
+	 */
 	public void setForeign(Boolean foreign) {
 		this.foreign = foreign;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public String getLanguage() {
 		return language;
 	}
 
+	/**
+	 *
+	 * @param language
+	 */
 	public void setLanguage(String language) {
 		this.language = language;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public Boolean getOvert() {
 		return overt;
 	}
 
+	/**
+	 *
+	 * @param overt
+	 */
 	public void setOvert(Boolean overt) {
 		this.overt = overt;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public List<String> getSynonyms() {
 		return synonyms;
 	}
 
+	/**
+	 *
+	 * @param synonyms
+	 */
 	public void setSynonyms(List<String> synonyms) {
 		this.synonyms = synonyms;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public List<String> getHypernyms() {
 		return hypernyms;
 	}
 
+	/**
+	 *
+	 * @param hypernyms
+	 */
 	public void setHypernyms(List<String> hypernyms) {
 		this.hypernyms = hypernyms;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public List<String> getHyponyms() {
 		return hyponyms;
 	}
 
+	/**
+	 *
+	 * @param hyponyms
+	 */
 	public void setHyponyms(List<String> hyponyms) {
 		this.hyponyms = hyponyms;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public List<EmbeddingObject> getEmbeddings() {
 		return embeddings;
 	}
 
+	/**
+	 *
+	 * @param embeddings
+	 */
 	public void setEmbeddings(List<EmbeddingObject> embeddings) {
 		this.embeddings = embeddings;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public List<List<Number>> getCharacterEmbeddings() {
 		return characterEmbeddings;
 	}
 
+	/**
+	 *
+	 * @param characterEmbeddings
+	 */
 	public void setCharacterEmbeddings(List<List<Number>> characterEmbeddings) {
 		this.characterEmbeddings = characterEmbeddings;
 	}
 
+	/**
+	 * This method returns the sentiment score for the token.
+	 *
+	 * @return
+	 */
 	public String getSentiment() {
 		return sentiment;
 	}
 
+	/**
+	 * This method sets the sentiment score for the token.
+	 *
+	 * @param sentiment
+	 */
 	public void setSentiment(String sentiment) {
 		this.sentiment = sentiment;
 	}
