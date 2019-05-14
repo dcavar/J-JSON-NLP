912e2a81024a130c494d1a58de09c244cb6df496
Aarushi <aarushiibisht@gmail.com>
2019-05-14 16:06:34 -0400

    Fixed nltk1.json, nltk2.json

---
+++ src/main/java/org/nlp_lab/jsonnlp/VerbFrames.java
@@ -0,0 +1,31 @@
+package org.nlp_lab.jsonnlp;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VerbFrames {
+
+    @JsonProperty("classId")
+    private String classId;
+    @JsonProperty("frames")
+    private List<Frames> frames = new ArrayList<>();
+
+    @JsonProperty("classId")
+    public String getClassId() {
+        return classId;
+    }
+    @JsonProperty("classId")
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+    @JsonProperty("frames")
+    public List<Frames> getFrames() {
+        return frames;
+    }
+    @JsonProperty("frames")
+    public void setFrames(List<Frames> frames) {
+        this.frames = frames;
+    }
+}
