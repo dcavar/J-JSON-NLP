84f5870cbcf6990c60f521c4cf5d2053d243bfc2
Damir Cavar <dcavar@me.com>
2019-07-30 14:20:43 -0400

    Fixed a Jackson serialization setting and warning about a deprecated function, added UTF-8 as the default encoding to the Maven config. DC

--- src/main/java/org/nlp_lab/jsonnlp/Label.java
+++ src/main/java/org/nlp_lab/jsonnlp/Label.java
@@ -7,7 +7,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "type",
     "label",
